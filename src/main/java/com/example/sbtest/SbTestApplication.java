6c692522c05ff5c9904ef2c9df4c2847da6b84d4
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-05-15 17:26:02 +0200

    M2-  @Value,@ConfigurationProperties refactor

--- src/main/java/com/example/sbtest/SbTestApplication.java
+++ src/main/java/com/example/sbtest/SbTestApplication.java
@@ -1,14 +1,11 @@
 package com.example.sbtest;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class SbTestApplication {
 
     public static void main(String[] args) {
